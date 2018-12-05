7c08a805b58807592c282a93cfca1dd39de75804
Alexander Shulzycki <AlexShulzycki@gmail.com>
2018-12-05 22:44:51 +0100

    divs for the game board created dynamically, however invisible

--- Project/battleships/public/javascripts/main.js
+++ Project/battleships/public/javascripts/main.js
@@ -1,7 +1,25 @@
 window.onload = initScript;
 
-
-
 function initScript(){
-  alert("working");
+  var playerBoard = document.getElementById('playerBoard');
+  var opponentBoard = document.getElementById('opponentBoard');
+
+  for(var i = 0; i<20; i++){
+
+    var newRow = document.createElement("div");
+    newRow.classList.add("row");
+
+    for(var a = 0; a<10; a++){
+      newBlock = document.createElement("div");
+      newBlock.classList.add("block");
+      newRow.appendChild(newBlock);
+    }
+    if(i<10){
+      playerBoard.appendChild(newRow);
+    }
+    else {
+      opponentBoard.appendChild(newRow);
+    }
+
+  }
 }
