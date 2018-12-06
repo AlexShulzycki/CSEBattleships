eee54a9546fe48070dcca86e53de52ab4ccd6841
Alexander Shulzycki <AlexShulzycki@gmail.com>
2018-12-06 18:15:32 +0100

    helper functions game setup, need for testing and debugging too tired rn

--- Project/battleships/app.js
+++ Project/battleships/app.js
@@ -27,6 +27,9 @@ var gameMap;
 
 function Game(players){
   this.players = players;
+  this.idBoard = new Map();
+  this.idBoard.set(players[0],this.boards[0]);
+  this.idBoard.set(players[1],this.boards[1]);
   this.state = "init";
   var tempboard =[[],[]];
   for(var x = 0; x < 20; x++){
@@ -42,9 +45,61 @@ function Game(players){
     }
 
   }
-  this.board = tempboard;
+  this.boards = tempboard;
+}
+//helper functions
+//switch pair of coordinate pairs
+game.switch(a,b,index){
+    var temp = a[index];
+    a[index] = b[index];
+    b[index] = temp;
+}
+
+//verify that space is empty;
+game.isEmpty(a,b,board){
+  //make sure order is correct to be fed into the loops
+  if(b[0]<a[0]){
+    game.switch(a,b,0);
+  }
+  if(b[1]<a[1]){
+    game.switch(a,b,1);
+  }
+
+
+  var empty = true;
+  for(var x = a[0]; x<b[0];x++){
+    for(var y = a[0]; y<b[1]; y++){
+      if(this.boards[board][x][y]!= "water"){
+        empty = false;
+      }
+    }
+  }
+}
+
+
+//create ship on game board
+game.validShips(id, a, b, type){
+  //Types: Frigate (1*5), Sub(1*3), Carrier(2*5), Destroyer(1*4)
+  var board = this.idBoard(id);
+  switch (type) {
+    case "Frigate":
+
+      break;
+    case "Sub":
+
+      break;
+    case "Carrier":
+
+      break;
+
+    case "Destroyer":
+
+      break;
+    default:
+      console.log("unknown ship type");
+  }
 }
 
 //test
-var game = new Game("none");
+var game = new Game([12,15]);
 console.log(game.board[0][9].length);
