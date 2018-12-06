1e89ab2ad90deae0489670bf7f820bb6846318ee
Alexander Shulzycki <AlexShulzycki@gmail.com>
2018-12-06 16:50:13 +0100

    created game object with game boards 2d arrays, todo player id mapping

--- Project/battleships/app.js
+++ Project/battleships/app.js
@@ -3,9 +3,48 @@ var http = require("http");
 var indexRouter = require("./routes/index");
 var port = process.argv[2];
 var app = express();
+var websocket = require("ws");
+
 
 app.use(express.static(__dirname + "/public"));
-http.createServer(app).listen(port);
+var server = http.createServer(app);
 
 //routers
 app.get("/*", indexRouter);
+
+
+//websockets, eg game logic
+const wss = new websocket.Server({server});
+
+wss.on("connection",function(){
+  console.log("connection established");
+});
+
+
+server.listen(port);
+
+var gameMap;
+
+function Game(players){
+  this.players = players;
+  this.state = "init";
+  var tempboard =[[],[]];
+  for(var x = 0; x < 20; x++){
+    var temprow = [];
+    for(var y = 0; y<10; y++){
+      temprow.push("water");
+    }
+
+    if(x<10){
+      tempboard[0].push(temprow);
+    }else{
+      tempboard[1].push(temprow);
+    }
+
+  }
+  this.board = tempboard;
+}
+
+//test
+var game = new Game("none");
+console.log(game.board[0][9].length);
