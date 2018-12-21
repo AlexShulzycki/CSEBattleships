a4368a5797c4580b0128043979cab39dacd2cf3f
Alexander Shulzycki <AlexShulzycki@gmail.com>
2018-12-21 22:29:34 +0100

    hastily put together framework for client recievin websockets

--- Project/battleships/public/javascripts/websocc.js
+++ Project/battleships/public/javascripts/websocc.js
@@ -6,17 +6,38 @@ webSocket.onmessage = function(event){
     console.log(data);
     switch (data.type) {
 
-      case 0:
+      case 0: //pairing
+        if(!game){
+          //still queued
+        }else{
+          //game found
+          player = data.player;
+        }
+        break;
+      case 1: // placing
+        //confirm that it is placed
+        break;
+      case 2:
+        if(data.player == player){
+          //own board hit
+        }else{
+          //opponents board hit
+        }
+        break;
+
+      case 5: //error
+        alert("Error: "+data.data);
+        break;
+
+      case 6: //// win
+        if(data.winner == player){
+          //player won
+        }else{
+          //you lost buckaroo
+        }
         break;
       default:
 
+
     }
-}
-
-function webSock(query, callback){
-    webSocket.send("4");
-}
-
-function getPlayers(){
-
 }
